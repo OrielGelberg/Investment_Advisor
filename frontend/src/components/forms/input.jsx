2c7d070524b3dcdedfb5d4c7d4e8532f5c461f2d
eazy <ezrirot@gmail.com>
2025-09-15 20:55:44 +0300

    input generic  form design

---
+++ frontend/src/components/forms/input.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Input({ type = "text", name, value, onChange, placeholder }) {
+  return (
+    <div className="">
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className=""
+      />
+    </div>
+  );
+}
