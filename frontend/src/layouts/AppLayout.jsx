e2900f36103e2cfeac74fcdaedbf509995a82058
eazy <ezrirot@gmail.com>
2025-09-16 15:31:06 +0300

    a basic page structure for default page design

---
+++ frontend/src/layouts/AppLayout.jsx
@@ -0,0 +1,18 @@
+
+export default function AppLayout({ header, footer, mainContent }) {
+    return (
+        <div className="min-h-screen flex flex-col bg-gray-50">
+            <header>
+                {header}
+            </header>
+
+            <main className="flex-1 p-6">
+                {mainContent}
+            </main>
+
+            <footer className="bg-gray-200 text-center p-4 text-sm">
+                {footer}
+            </footer>
+        </div>
+    );
+}
