ddb68a3d0786f880941353bed92ba3c0c1f1f335
eazy <ezrirot@gmail.com>
2025-09-18 10:22:34 +0300

    fixed params name and initializing

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -4,20 +4,17 @@ import Footer from "../components/Footer";
 const defaultHeader = Header;
 const defaultFooter = Footer
 
-export default function AppLayout({ header=defaultHeader, footer=defaultFooter, mainContent }) {
+export default function AppLayout({ header: Header, footer: Footer, mainContent: MainContent }) {
     return (
         <div className="min-h-screen flex flex-col bg-gray-50">
-            <Header>
-                {<header/>}
-            </Header>
+            <Header />
+
 
             <main className="flex-1 p-6">
-                {<mainContent/>}
+                {<MainContent />}
             </main>
 
-            <footer className="bg-gray-200 text-center p-4 text-sm">
-                {<footer/>}
-            </footer>
+            <Footer />
         </div>
     );
 }
